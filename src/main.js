68b801bdba5bc259412cdac8f9aa31b8d4c035b5
Alex <2061alex@gmail.com>
2026-06-05 21:13:06 +0300

    feat: wire Nav, BookingForm, BeforeAfter into app entry

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import './style.css'
 import Experience from './webgl/Experience.js'
 import Cursor from './ui/Cursor.js'
 import Preloader from './ui/Preloader.js'
+import Nav from './ui/Nav.js'
+import BookingForm from './ui/BookingForm.js'
+import BeforeAfter from './ui/BeforeAfter.js'
 
 const experience = new Experience(document.querySelector('canvas.webgl'))
 const cursor = new Cursor()
 const preloader = new Preloader(experience)
+const nav = new Nav()
+const bookingForm = new BookingForm()
+const beforeAfter = new BeforeAfter()
